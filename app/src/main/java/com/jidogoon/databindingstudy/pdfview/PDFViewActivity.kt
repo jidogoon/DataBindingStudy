5eba15bac296f3d04866a9570d3a2b7eaff3b2c6
jidogoon <jidohyun@gmail.com>
2018-09-18 23:58:53 +0900

    make pdfViewActivity using dataBinding

--- app/src/main/java/com/jidogoon/databindingstudy/pdfview/PDFViewActivity.kt
+++ app/src/main/java/com/jidogoon/databindingstudy/pdfview/PDFViewActivity.kt
@@ -4,23 +4,25 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.jidogoon.databindingstudy.R
-import com.jidogoon.pdfrendererview.PdfRendererView
+import com.jidogoon.databindingstudy.databinding.ActivityPdfviewBinding
 
 class PDFViewActivity: AppCompatActivity() {
     companion object {
         private const val KEY_URL = "KEY_URL"
         fun startActivity(activity: Activity, url: String) {
-            val intent = Intent(activity, PDFViewActivity::class.java)
-            intent.putExtra(KEY_URL, url)
+            val intent = Intent(activity, PDFViewActivity::class.java).apply {
+                putExtra(KEY_URL, url)
+            }
             activity.startActivity(intent)
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pdfview)
+        val binding: ActivityPdfviewBinding = DataBindingUtil.setContentView(this, R.layout.activity_pdfview)
         val url = intent.getStringExtra(KEY_URL)
-        findViewById<PdfRendererView>(R.id.pdfView).initWithUrl(url)
+        binding.pdfView.initWithUrl(url)
     }
 }
